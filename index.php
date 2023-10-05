4601898413c8a93656f58da08b52e7c05609577d
Mioriarty <moritz.seppelt@googlemail.com>
2023-10-05 10:44:15 +0200

    improved xss

--- index.php
+++ index.php
@@ -4,9 +4,18 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Notes WG</title>
+    <script>
+        function search() {
+            const urlParams = new URLSearchParams(window.location.search);
+            urlParams.set('search', document.getElementById("search").value);
+            window.location.search = urlParams;
+        }
+    </script>
 </head>
 <body>
     <h1>Notes WG</h1>
+    <input placeholder="Key words" id="search" />
+    <button onclick="search();">Search</button>
     <p>Get Param (search):<?= $_GET['search'] ?></p>
 </body>
 </html>
