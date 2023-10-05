2a3ac9cb4a0a25b84ea688c0091a77102a5d6839
Mioriarty <moritz.seppelt@googlemail.com>
2023-10-05 10:23:43 +0200

    add xss

--- index.php
+++ index.php
@@ -1,3 +1,12 @@
-<?php
-
-echo "Hello World!";
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Notes WG</title>
+</head>
+<body>
+    <h1>Notes WG</h1>
+    <p>Get Param (search):<?= $GET['serach'] ?></p>
+</body>
+</html>
